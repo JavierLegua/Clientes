50270f9a6f02bd81a5f3ae7fdda410e325328b6b
JavierLegua <javierlegua14@gmail.com>
2021-04-14 11:50:10 +0200

    Inicio interfaz gráfica

---
+++ src/Modelo/Cliente.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author alumnodaw
+ */
+public class Cliente {
+    
+    private String cliente;
+    private String apellido1;
+    private String apellido2;
+    private String estadoCivil;
+    private String sexo;
+    private int edad;
+
+    public Cliente(String cliente, String apellido1, String apellido2, String estadoCivil, String sexo, int edad) {
+        this.cliente = cliente;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.estadoCivil = estadoCivil;
+        this.sexo = sexo;
+        this.edad = edad;
+    }
+
+    /**
+     * @return the cliente
+     */
+    public String getCliente() {
+        return cliente;
+    }
+
+    /**
+     * @param cliente the cliente to set
+     */
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    /**
+     * @return the apellido1
+     */
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    /**
+     * @param apellido1 the apellido1 to set
+     */
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    /**
+     * @return the apellido2
+     */
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    /**
+     * @param apellido2 the apellido2 to set
+     */
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    /**
+     * @return the estadoCivil
+     */
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    /**
+     * @param estadoCivil the estadoCivil to set
+     */
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "cliente=" + cliente + ", apellido1=" + apellido1 + ", apellido2=" + apellido2 + ", estadoCivil=" + estadoCivil + ", sexo=" + sexo + ", edad=" + edad + '}';
+    }
+    
+        
+}
